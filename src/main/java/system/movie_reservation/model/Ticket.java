172146b0489b5f113a19f91547ec39a0898db40d
Luis Felipe <filipinho_v3@hotmail.com>
2025-03-18 16:30:44 -0300

    feat: Ticket Class created

---
+++ src/main/java/system/movie_reservation/model/Ticket.java
@@ -0,0 +1,25 @@
+package system.movie_reservation.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import system.movie_reservation.model.Enums.MovieTime;
+
+@Entity
+@Table(name = "ticket_tb")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private User user;
+    private Movie movie;
+    private MovieTime movieTime;
+    private Seat seat;
+}
