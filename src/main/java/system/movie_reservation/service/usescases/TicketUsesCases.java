2e65e4043a361d5ea51798ebc8de94be87a47a34
Luis Felipe <filipinho_v3@hotmail.com>
2025-03-26 16:00:15 -0300

    feat: New Classe TicketUsesCases

--- src/main/java/system/movie_reservation/service/usescases/TicketUsesCases.java
+++ src/main/java/system/movie_reservation/service/usescases/TicketUsesCases.java
@@ -1,8 +1,23 @@
 package system.movie_reservation.service.usescases;
 
 import system.movie_reservation.model.ticket.Ticket;
+import system.movie_reservation.model.ticket.TicketRequest;
+import system.movie_reservation.model.ticket.TicketRequestUpdate;
+import system.movie_reservation.model.ticket.TicketResponse;
+
+import java.util.List;
 
 public interface TicketUsesCases {
 
     public Ticket getTicketById(Long id);
+
+    public TicketResponse createTicket(TicketRequest ticketRequest);
+
+    public List<TicketResponse> geAllTickets();
+
+    public TicketResponse updateTicket(TicketRequestUpdate ticketReqUpdate);
+
+    public String deleteTicketById(Long id);
+
+    public String deleteAllTickets();
 }
